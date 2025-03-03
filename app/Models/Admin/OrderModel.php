1c41d2a4a38ba95877134b9e9fe0a35a69d6bc75
TienHoang472005 <kurosama0407@gmail.>
2025-03-03 22:30:32 +0700

    thêm xem doanh thu số đơn hàng trong dashboard

--- app/Models/Admin/OrderModel.php
+++ app/Models/Admin/OrderModel.php
@@ -1,19 +1,23 @@
 <?php
 
-class OrderModel{
+class OrderModel
+{
     public $db;
-    public function __construct(){
+    public function __construct()
+    {
         $this->db = new Database();
     }
 
-    public function getAllOrder(){
+    public function getAllOrder()
+    {
         $sql = "select * from `order`";
         $stmt = $this->db->pdo->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll();
     }
 
-    public function getOrderDetail(){
+    public function getOrderDetail()
+    {
         $order_id = $_GET['order_id'];
         $sql = "SELECT order_detail.*, products.name, products.image_main, (order_detail.quantity * order_detail.price) AS total FROM `order_detail` JOIN products on order_detail.product_id = products.id WHERE order_detail.order_id = :order_id";
         $stmt = $this->db->pdo->prepare($sql);
@@ -21,8 +25,9 @@ class OrderModel{
         $stmt->execute();
         return $stmt->fetchAll();
     }
-    
-    public function changeStatusModel(){
+
+    public function changeStatusModel()
+    {
         $order_id = $_POST['order_id'];
         $status = $_POST['status'];
 
@@ -31,5 +36,24 @@ class OrderModel{
         $stmt->bindParam(':status', $status);
         $stmt->bindParam(':order_id', $order_id);
         return $stmt->execute();
+    }
+
+    public function doanhThu()
+    {
+        $sql = "SELECT SUM(total) AS revenue FROM `order` WHERE status = 'completed'";
+        $stmt = $this->db->pdo->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result ? $result['revenue'] : 0;
+    }
+
+    public function tongDonHang()
+    {
+        $sql = "SELECT COUNT(*) AS total_orders FROM `order` WHERE status = 'completed'";
+        $stmt = $this->db->pdo->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result ? (int) $result['total_orders'] : 0;
     }
 }
